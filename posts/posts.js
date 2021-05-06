ad8e08a1ee017b429ee5b144c4868d1a3c420e33
Jamie Martines <dev.jpm@gmail.com>
2021-05-06 10:44:31 -0400

    add post data as json

---
+++ posts/posts.js
@@ -0,0 +1,56 @@
+export default posts = [
+  {
+    id: '1',
+    name: 'Lift Communities',
+    url: 'https://liftcommunities.org',
+    imageUrl: ''
+  },
+  {
+    id: '2',
+    name: 'NBA Hoop Magazine',
+    url: 'https://hoop.nba.com/',
+    imageUrl: ''
+  },
+  {
+    id: '3',
+    name: 'JED Foundation',
+    url: 'https://jedfoundation.com/',
+    imageUrl: ''
+  },
+  {
+    id: '4',
+    name: 'JED Set to Go',
+    url: 'https://www.settogo.org/',
+    imageUrl: ''
+  },
+  {
+    id: '5',
+    name: 'American Medical Association',
+    url: 'https://partners.wsj.com/ama/charting-change/',
+    imageUrl: ''
+  },
+  {
+    id: '6',
+    name: 'JP Morgan Asset Management',
+    url: 'https://partners.wsj.com/jpmam/lets-talk/',
+    imageUrl: ''
+  },
+  {
+    id: '7',
+    name: 'Brighthouse Financial',
+    url: 'https://partners.wsj.com/brighthouse-financial/new-light-on-retirement/',
+    imageUrl: ''
+  },
+  {
+    id: '8',
+    name: 'OKL Palette',
+    url: 'https://www.onekingslane.com/p/4792665-marie-accent-chair.do',
+    imageUrl: ''
+  },
+  {
+    id: '9',
+    name: 'Spring Health',
+    url: 'https://springhealth.com',
+    imageUrl: ''
+  },
+]
