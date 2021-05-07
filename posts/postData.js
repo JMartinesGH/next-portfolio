833034d09b3f8f97e07a63cef71ed3419f16e5bb
Jamie Martines <dev.jpm@gmail.com>
2021-05-06 22:12:12 -0400

    renamed images in hopes to fix bug with Netlify large media

--- posts/postData.js
+++ posts/postData.js
@@ -3,54 +3,54 @@ export const postData = [
     "id": "1",
     "name": "Lift Communities",
     "url": "https://liftcommunities.org",
-    "imageUrl": "/images/liftcommunities.gif"
+    "imageUrl": "/images/lift.gif"
   },
   {
     "id": "2",
     "name": "NBA Hoop Magazine",
     "url": "https://hoop.nba.com/",
-    "imageUrl": "/images/hoops.gif"
+    "imageUrl": "/images/nbahoops.gif"
   },
   {
     "id": "3",
     "name": "JED Foundation",
     "url": "https://jedfoundation.com/",
-    "imageUrl": "/images/jed.gif"
+    "imageUrl": "/images/jedfoundation.gif"
   },
   {
     "id": "4",
     "name": "JED Set to Go",
     "url": "https://www.settogo.org/",
-    "imageUrl": "/images/jed.gif"
+    "imageUrl": "/images/jedfoundation.gif"
   },
   {
     "id": "5",
     "name": "American Medical Association",
     "url": "https://partners.wsj.com/ama/charting-change/",
-    "imageUrl": "/images/WSJCS_AMA.gif"
+    "imageUrl": "/images/wsjcs-ama.gif"
   },
   {
     "id": "6",
     "name": "JP Morgan Asset Management",
     "url": "https://partners.wsj.com/jpmam/lets-talk/",
-    "imageUrl": "/images/WSJCS_JPMAM.gif"
+    "imageUrl": "/images/wsjcs-jpmam.gif"
   },
   {
     "id": "7",
     "name": "Brighthouse Financial",
     "url": "https://partners.wsj.com/brighthouse-financial/new-light-on-retirement/",
-    "imageUrl": "/images/WSJCS_Brighthouse.gif"
+    "imageUrl": "/images/wsjcs-brighthouse.gif"
   },
   {
     "id": "8",
     "name": "OKL Palette",
     "url": "https://www.onekingslane.com/p/4792665-marie-accent-chair.do",
-    "imageUrl": "/images/palette.gif"
+    "imageUrl": "/images/oklpalette.gif"
   },
   {
     "id": "9",
     "name": "Spring Health",
     "url": "https://springhealth.com",
-    "imageUrl": "/images/springhealth.gif"
+    "imageUrl": "/images/springhealthhome.gif"
   }
 ]
