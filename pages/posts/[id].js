d2733af11bd6ae50b8815cb25606ff67c71e269f
Jamie Martines <dev.jpm@gmail.com>
2021-05-07 10:33:21 -0400

    added catch for image arrays in the data object, display carousel if object, added images for aruba project.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -6,6 +6,9 @@ import Nav from '../../components/Nav'
 import Pagination from '../../components/Pagination'
 import GlobalStyles from '../../components/styles/GlobalStyles'
 
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+
 const PostStyled = styled.div`
 margin: 0 auto;
 max-width: var(--maxWidth);
@@ -33,9 +36,20 @@ h1{
   }
 }
 
-img{
+div.carousel-wrapper{
   max-width: var(--maxWidth);
   width: 100%;
+  max-height: 563px;
+  min-height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+img{
+  width: 100%;
+  height: 100%;
+  object-fit: scale-down;
 }
 `
 
@@ -46,6 +60,26 @@ const PostDetails = styled.div`
   color: var(--white, #fff);
   padding: 1rem;
 `
+
+const responsive = {
+  superLargeDesktop: {
+    // the naming can be any, depends on you.
+    breakpoint: { max: 4000, min: 3000 },
+    items: 1
+  },
+  desktop: {
+    breakpoint: { max: 3000, min: 1024 },
+    items: 1
+  },
+  tablet: {
+    breakpoint: { max: 1024, min: 464 },
+    items: 1
+  },
+  mobile: {
+    breakpoint: { max: 464, min: 0 },
+    items: 1
+  }
+}
 
 export default function Post({ postData }) {
   postData = postData[0]
@@ -63,16 +97,28 @@ export default function Post({ postData }) {
         <article>
             <h1>
               <span>{postData.name}</span>
-              <span className='link-out'><a href={postData.url}>↗</a></span>
+              {postData.url && (
+                <span className='link-out'><a href={postData.url}>↗</a></span>
+              )}
             </h1>
-          {/* <Image
-            src={postData.imageUrl}
-            alt={postData.name}
-            width="1000"
-            height="563"
-            layout="responsive"
-          /> */}
-          <img src={postData.imageUrl} alt={postData.name}/>
+          {typeof postData.imageUrl === "string" && (
+            <img src={postData.imageUrl} alt={postData.name}/>
+          )}
+          {typeof postData.imageUrl === "object" && (
+            <Carousel
+              swipeable={true}
+              draggable={true}
+              responsive={responsive}
+              ssr={true}
+              infinite={true}
+            >
+              {postData.imageUrl.map(element => (
+                <div key={element} className="carousel-wrapper">
+                  <img src={element} alt={postData.name} />
+                </div>
+              ))}
+            </Carousel>
+          )}
         </article>
         <Pagination page={page} count={pageCount} />
       </PostStyled>
