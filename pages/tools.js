16bbdc264ce943c4f0b6911a44ecbf7d19f3560b
Jamie Martines <dev.jpm@gmail.com>
2021-05-07 12:08:42 -0400

    More tweaks to copy on the tools page

--- pages/tools.js
+++ pages/tools.js
@@ -8,6 +8,11 @@ const Tools = styled.div`
 margin: 0 auto;
 max-width: var(--maxWidth);
 min-height: 100vh;
+
+a{
+  text-decoration: underline;
+  font-weight: bold;
+}
 `
 
 export default function ToolsPage() {
@@ -20,7 +25,7 @@ export default function ToolsPage() {
       <Nav />
       <Tools>
         <p>
-          For this presentation I used Next.js and I'm serving the site via Netlify. Other tools I'm using are Styled Components for CSS in JS, NProgress for progress bars, and react-multi-carousel for a carousel on the pages with multiple images. The animated gradient is based on a Code Pen linked below. 
+          For this presentation I used <a href="https://nextjs.org/" target="_blank">Next.js</a> and I'm serving the site via <a href="ttps://www.netlify.com/" target="_blank">Netlify</a>. Other tools I'm using are <a href="https://styled-components.com/" target="_blank">Styled Components</a> for CSS in JS, <a href="https://ricostacruz.com/nprogress/" target="_blank">NProgress</a> for progress bars, and <a href="https://www.npmjs.com/package/react-multi-carousel" target="_blank">react-multi-carousel</a> for a carousel on the pages with multiple images. The animated gradient is based on a this <a href="https://codepen.io/P1N2O/pen/pyBNzX" target="_blank">Code Pen</a>. 
         </p>
         <ul>
           <li>
@@ -46,6 +51,16 @@ export default function ToolsPage() {
           <li>
             <a href="https://codepen.io/P1N2O/pen/pyBNzX" target="_blank">
               Code Pen - Pure CSS Animated Gradient Background
+            </a>
+          </li>
+          <li>
+            <a href="https://www.netlify.com/" target="_blank">
+              Netlify
+            </a>
+          </li>
+          <li>
+            <a href="https://docs.netlify.com/large-media/overview/" target="_blank">
+              Netlify - Large Media
             </a>
           </li>
         </ul>
