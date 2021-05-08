800c8016cc740dd6e0bbbc91a8748a4dd14499ee
Jamie Martines <dev.jpm@gmail.com>
2021-05-08 13:51:33 -0400

    images using netlify transformations were no longer animated, removed transforms on desktop

--- components/Picture.js
+++ components/Picture.js
@@ -15,8 +15,8 @@ const PictureStyled = styled.picture`
 export default function Picture({ url, alt }) {
   return (
     <PictureStyled>
-      <source srcset={`${url}?nf_resize=fit&w=1000`} media="(min-width: 1024px)" />
-      <source srcset={`${url}?nf_resize=fit&w=700`} media="(min-width: 740px)" />
+      <source srcset={`${url}`} media="(min-width: 1024px)" />
+      <source srcset={`${url}`} media="(min-width: 740px)" />
       <img src={`${url}?nf_resize=fit&w=500`} alt={alt} />
     </PictureStyled>
   )
